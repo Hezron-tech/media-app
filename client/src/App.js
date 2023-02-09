bd8037ab17c879944b4ea4a8465ba24433b54e4c
Hezron-tech <kiprophez@gmail.com>
2023-02-09 22:57:37 +0300

    work on client server

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,21 @@
+import {Container, AppBar,Typography,Grow,Grid} from '@material-ui/core' 
+import memories from './images/memories.jpeg'
 
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Working with REACT and NODE app</h1>
-    </div>
+   <Container maxWidth='lg'>
+    <AppBar position='static' color='inherit'>
+      <Typography variant='h2' align='center'>Memories</Typography>
+
+      <img src={memories} alt="memories" height="300"/>
+
+    </AppBar>
+
+
+
+   </Container>
   );
 }
 
