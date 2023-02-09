64933ea92fc83f79089a8cb8a9bb210231692aa1
Hezron-tech <kiprophez@gmail.com>
2023-02-09 23:13:34 +0300

    styling the post

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,40 @@
-import {Container, AppBar,Typography,Grow,Grid} from '@material-ui/core' 
-import memories from './images/memories.jpeg'
+import { Container, AppBar, Typography, Grow, Grid } from "@material-ui/core";
+import memories from "./images/memories.jpeg";
+import Form from "./components/Form/Form";
+import Posts from "./components/Posts/post";
 
-import './App.css';
+
+import "./App.css";
 
 function App() {
   return (
-   <Container maxWidth='lg'>
-    <AppBar position='static' color='inherit'>
-      <Typography variant='h2' align='center'>Memories</Typography>
+    <Container maxWidth="lg">
+      <AppBar position="static" color="inherit">
+        <Typography variant="h2" align="center">
+          Memories
+        </Typography>
 
-      <img src={memories} alt="memories" height="300"/>
+        <img src={memories} alt="memories" height="300" />
+      </AppBar>
 
-    </AppBar>
-
-
-
-   </Container>
+      <Grow in>
+        <Container>
+          <Grid
+            container
+            justifyContent="space-between"
+            alignItems="stretch"
+            spacing={3}
+          >
+            <Grid item xs={12} sm={7}>
+              <Posts />
+            </Grid>
+            <Grid item xs={12} sm={4}>
+              <Form />
+            </Grid>
+          </Grid>
+        </Container>
+      </Grow>
+    </Container>
   );
 }
 
---
+++ client/src/components/Posts/post.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Post from "./Post/post";
+
+const Posts =()=>{
+    return(
+
+        <>
+        <h1>Posts</h1>
+        <post/>
+        <Post/>
+        
+        </>
+        
+    )
+}
+
+export default Posts
